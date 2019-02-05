8a93a20c014c711e7b59607d1b84ab2b884e4eaa
Rodrigo Nascimento Hernandez <rodrigo.freebsd@minasambiente.com.br>
2019-02-05 02:34:25 -0200

    Interesting test comparing Py27 vs Py36 under heavy threading 'provation'

---
+++ Py27vsPy36-threadingspree/threadingspree.py
@@ -0,0 +1,61 @@
+import threading
+import time
+import random
+import atexit
+
+cc = list()
+ct1 = 0
+ct2 = 0
+ct3 = 0
+ct4 = 0
+locklist = False
+lockcontrol = True
+
+def main():
+    v0 = 0
+    v1 = 0
+    cc.append(v0)
+    cc.append(v1)
+    for x in range(0, 1000):
+        t = threading.Thread(target=_cat, )
+        t.start()
+
+def _cat():
+    global ct1
+    global ct2
+    global ct3
+    global ct4
+    global cc
+    global locklist
+    global lockcontrol
+
+    if lockcontrol:
+        while locklist:
+            time.sleep(0.01)
+
+    locklist = True
+    randn = random.random() / 100
+    v0 = cc[0]
+    v1 = cc[1]
+    v0 = 1 - v0
+    v1 = 1 - v1
+    cc[0] = v0
+    time.sleep(randn)
+    cc[1] = v1
+    if cc == [0, 0]:
+        ct1 += 1
+    if cc == [1, 0]:
+        ct2 += 1
+    if cc == [0, 1]:
+        ct3 += 1
+    if cc == [1, 1]:
+        ct4 += 1
+    locklist = False
+
+@atexit.register
+def _results():
+    print ("[0, 0] = " + str(ct1) + " | " + "[1, 0] = " + str(ct2) + " | " + "[0, 1] = " + str(ct3) + " | " + "[1, 1] = " + str(ct4))
+    print("Total processed: " + str(ct1+ct2+ct3+ct4))
+
+if __name__ == "__main__":
+	main()
